720b543a2d4522c407cc9db3f9c9edb6505fb97a
MCamilaG <camilagomezr05@gmail.com>
2017-03-09 12:50:31 -0500

    avance AlquilerItemsBean

--- src/main/java/edu/eci/pdsw/samples/managedbeans/AlquilerItemsBean.java
+++ src/main/java/edu/eci/pdsw/samples/managedbeans/AlquilerItemsBean.java
@@ -5,9 +5,12 @@
  */
 package edu.eci.pdsw.samples.managedbeans;
 
+import edu.eci.pdsw.samples.entities.Cliente;
 import edu.eci.pdsw.samples.entities.ItemRentado;
+import edu.eci.pdsw.samples.services.ExcepcionServiciosAlquiler;
 import edu.eci.pdsw.samples.services.ServiciosAlquiler;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -19,6 +22,8 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean(name = "AlquilerItems")
 @SessionScoped
 public class AlquilerItemsBean implements Serializable {
+
+    
     ServiciosAlquiler sp = ServiciosAlquiler.getInstance();
     long multas=0;
     
@@ -32,6 +37,20 @@ public class AlquilerItemsBean implements Serializable {
         multas=multa;
     }
     
+    public List<Cliente> getClientes() throws ExcepcionServiciosAlquiler{
+        List<Cliente> clientes= new ArrayList<Cliente>();
+        clientes = sp.consultarClientes();
+        return  clientes;
+    }
+    
+    public List<ItemRentado> getRentados(){
+        List<ItemRentado> rentados= new ArrayList<ItemRentado>();
+        return  rentados;
+    }
+    
+    
+    
+     
     
 
 }
